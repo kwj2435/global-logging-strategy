8ee341956733794167f40c7681f434997028be4d
kimuijin <dmlwls2435@naver.com>
2024-12-05 21:48:30 +0900

    Initial commit

---
+++ src/main/java/com/study/logstudy/GlobalLoggingStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.study.logstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalLoggingStrategyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GlobalLoggingStrategyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/study/logstudy/GlobalLoggingStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.logstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalLoggingStrategyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
