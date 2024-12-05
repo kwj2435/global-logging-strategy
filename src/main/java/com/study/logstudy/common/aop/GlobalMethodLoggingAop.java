1b8311ca1b34d02a56bfc581a1122ed53610e74c
kimuijin <dmlwls2435@naver.com>
2024-12-05 21:51:35 +0900

    feat. add GlobalMethodLogging, GlobalRequestResponseLogging

---
+++ src/main/java/com/study/logstudy/common/aop/GlobalMethodLoggingAop.java
@@ -0,0 +1,24 @@
+package com.study.logstudy.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class GlobalMethodLoggingAop {
+
+  @Before("execution(* com.study..*.*(..))")
+  public void logMethodCall(JoinPoint joinPoint) {
+    String fullClassName = joinPoint.getSignature().getDeclaringTypeName();
+    String className = fullClassName.substring(fullClassName.lastIndexOf('.') + 1);
+    String methodName = joinPoint.getSignature().getName();
+    Object[] args = joinPoint.getArgs();
+
+    log.info(">>>> Call Method: {}.{} - Args: {}", className, methodName, args);
+  }
+}
+
---
+++ src/main/java/com/study/logstudy/common/filter/GlobalReqResLoggingFilter.java
@@ -0,0 +1,45 @@
+package com.study.logstudy.common.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class GlobalReqResLoggingFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, jakarta.servlet.ServletException {
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+    log.info(">>>> Request URI: {}, Request Method: {}, Request Headers: {}, Request Body: {}",
+        httpRequest.getRequestURI(), httpRequest.getMethod(),
+        httpRequest.getHeaderNames(), getRequestBody(httpRequest)
+    );
+
+    chain.doFilter(request, response);
+
+    log.info(">>>> Response Status: {}", httpResponse.getStatus());
+  }
+
+  private String getRequestBody(HttpServletRequest request) {
+    try {
+      BufferedReader reader = request.getReader();
+      return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+    } catch (IOException e) {
+      log.error("Failed to read request body", e);
+      return "Error reading request body";
+    }
+  }
+}
+
